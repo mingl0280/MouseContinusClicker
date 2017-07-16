27f7685766710a3d4694b668f090067ac8e1f1ca
pmx <mingl0280@gmail.com>
2017-07-16 15:45:04 -0600

    Move MsgReceivedEventArgs to separate file.
    Signed-off-by: pmx <mingl0280@gmail.com>

--- MouseClicker/ClassThreading.cs
+++ MouseClicker/ClassThreading.cs
@@ -81,21 +81,4 @@ namespace MouseClicker
         }
         
     }
-
-    /// <summary>
-    /// Extended event args for message transmitting.
-    /// </summary>
-    public class MsgReceivedEventArgs: EventArgs
-    {
-        public int msg { get; set; }
-
-        /// <summary>
-        /// initializer
-        /// </summary>
-        /// <param name="i">message</param>
-        public MsgReceivedEventArgs(int i)
-        {
-            msg = i;
-        }
-    }
 }
---
+++ MouseClicker/MsgReceivedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MouseClicker
+{
+    /// <summary>
+    /// Extended event args for message transmitting.
+    /// </summary>
+    public class MsgReceivedEventArgs: EventArgs
+    {
+        public int msg { get; set; }
+
+        /// <summary>
+        /// initializer
+        /// </summary>
+        /// <param name="i">message</param>
+        public MsgReceivedEventArgs(int i)
+        {
+            msg = i;
+        }
+    }
+}
